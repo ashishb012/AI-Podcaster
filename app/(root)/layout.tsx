812ac59196ed62d17511fa414eb6e79993b2dd5b
ashishb012 <ashishb0128@gmail.com>
2024-07-06 13:18:26 +0530

    initial folder setup

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import "@/app/globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +16,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <main>
+          <p className="text-white-1">Left sidebar</p>
+          {children}
+          <p className="text-white-1">Right sidebar</p>
+        </main>
+      </body>
     </html>
   );
 }
