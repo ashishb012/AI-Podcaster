30f5361072392adbe9f8343312a06a1f1bcd5c46
ashishb012 <ashishb0128@gmail.com>
2024-07-06 14:23:07 +0530

    sidebars & convex

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lovely-flamingo-139.convex.cloud",
+      },
+      {
+        protocol: "https",
+        hostname: "standing-akita-26.convex.cloud",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
