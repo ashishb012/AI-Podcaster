eee6dc19972372b202641f2c966c5d05ecb45387
ashishb012 <ashishb0128@gmail.com>
2024-07-07 12:27:10 +0530

    components

---
+++ app/(root)/profile/[profile_id]/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Profile = ({
+  params: { profile_id },
+}: {
+  params: { profile_id: string };
+}) => {
+  return <div>Profile</div>;
+};
+
+export default Profile;
--- next.config.mjs
+++ next.config.mjs
@@ -10,6 +10,10 @@ const nextConfig = {
         protocol: "https",
         hostname: "standing-akita-26.convex.cloud",
       },
+      {
+        protocol: "https",
+        hostname: "img.clerk.com",
+      },
     ],
   },
 };
